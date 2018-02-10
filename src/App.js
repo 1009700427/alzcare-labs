fbd64ff592857f45068a2c0c9ca9603e823a9c0a
Steven Xu <siyuanx@usc.edu>
2018-02-10 11:11:41 -0800

    finished functionalities

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FormGroup, ControlLabel, FormControl, Form, Button } from 'react-bootstrap';
+import $ from 'jquery';
+
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            searchTerm: ""
+        }
+    }
+    // the function that searches for the image
+    searchImage(e) {
+        e.preventDefault();
+        $("#images").html("");
+        let searchTerm = this.state.searchTerm;
+        let flickrURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags="+this.state.searchTerm+"&format=json&jsoncallback=?";
+        $.getJSON(flickrURL)
+            .done(function(data){
+                if(data.items.length===0){
+                    $('#images').append("Nothing Found!");
+                }
+                $.each(data.items, function(i, item){
+                    $('<div/>', {
+                        class: "img-data",
+                        id: i
+                    }).appendTo("#images");
+                    $("<h5>").append(item.title).appendTo("#"+i);
+                    $("<img>").attr("src", item.media.m).appendTo("#"+i);
+                })
+            });
+    };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <Form inline action="/" method="POST" onSubmit={(e) => {this.searchImage(e)}}>
+                <FormGroup>
+                    <ControlLabel>Search for Image!</ControlLabel>{' '}
+                    <FormControl type="text" placeholder="Input Text Here" onChange={(e) => {
+                        this.setState({searchTerm: e.target.value});
+                    }}/>
+                </FormGroup>{' '}
+                <Button bsStyle="success" type="submit">Search</Button>
+            </Form>
+            <div id="images">
+            </div>
       </div>
     );
   }
